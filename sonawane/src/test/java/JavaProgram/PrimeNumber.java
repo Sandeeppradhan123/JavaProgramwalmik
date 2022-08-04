709f8ccda5c6485e2bf0050ae75d1bca31911bc3
SANDIP <SANDIP@DESKTOP-RMEHIQE>
2022-08-04 20:10:49 +0530

    2nd comit

--- sonawane/src/test/java/JavaProgram/PrimeNumber.java
+++ sonawane/src/test/java/JavaProgram/PrimeNumber.java
@@ -2,6 +2,23 @@ package JavaProgram;
 
 public class PrimeNumber {
 
-	
-	
+	public static void main(String[] args) {
+		int count = 0;
+		for (int i = 0; i < 101; i++) {
+			count = 0;
+			for (int j = 0; j < 101; j++) {
+
+				if (i % j == 0) {
+					count++;
+				}
+			}
+
+			if (count == 2) {
+            System.out.println(i);
+			}
+
+		}
+
+	}
+
 }
